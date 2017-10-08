c4e4994ecad457c683d6a86479237370a31fb146
Tautvydas Duda <ltdu@users.noreply.github.com>
2017-10-08 13:42:39 +0300

    implemented sample from: https://docs.microsoft.com/en-us/dotnet/framework/windows-workflow-foundation/rehosting-the-workflow-designer

--- SPWorkflowDesigner/MainWindow.xaml.cs
+++ SPWorkflowDesigner/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Activities.Core.Presentation;
+using System.Activities.Presentation;
+using System.Activities.Presentation.Toolbox;
+using System.Activities.Statements;
 
 namespace SPWorkflowDesigner
 {
@@ -20,9 +12,80 @@ namespace SPWorkflowDesigner
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WorkflowDesigner wd;
+
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            // Register the metadata  
+            this.RegisterMetadata();
+
+            // Add the WFF Designer  
+            this.AddDesigner();
+
+            this.AddToolBox();
+
+            this.AddPropertyInspector();
+        }
+
+        private void AddDesigner()
+        {
+            //Create an instance of WorkflowDesigner class.  
+            this.wd = new WorkflowDesigner();
+
+            //Place the designer canvas in the middle column of the grid.  
+            Grid.SetColumn(this.wd.View, 1);
+
+            //Load a new Sequence as default.  
+            this.wd.Load(new Sequence());
+
+            //Add the designer canvas to the grid.  
+            grid1.Children.Add(this.wd.View);
+        }
+
+        private void RegisterMetadata()
+        {
+            DesignerMetadata dm = new DesignerMetadata();
+            dm.Register();
+        }
+
+        private ToolboxControl GetToolboxControl()
+        {
+            // Create the ToolBoxControl.  
+            ToolboxControl ctrl = new ToolboxControl();
+
+            // Create a category.  
+            ToolboxCategory category = new ToolboxCategory("category1");
+
+            // Create Toolbox items.  
+            ToolboxItemWrapper tool1 =
+                new ToolboxItemWrapper("System.Activities.Statements.Assign",
+                typeof(Assign).Assembly.FullName, null, "Assign");
+
+            ToolboxItemWrapper tool2 = new ToolboxItemWrapper("System.Activities.Statements.Sequence",
+                typeof(Sequence).Assembly.FullName, null, "Sequence");
+
+            // Add the Toolbox items to the category.  
+            category.Add(tool1);
+            category.Add(tool2);
+
+            // Add the category to the ToolBox control.  
+            ctrl.Categories.Add(category);
+            return ctrl;
+        }
+
+        private void AddToolBox()
+        {
+            ToolboxControl tc = GetToolboxControl();
+            Grid.SetColumn(tc, 0);
+            grid1.Children.Add(tc);
+        }
+
+        private void AddPropertyInspector()
+        {
+            Grid.SetColumn(wd.PropertyInspectorView, 2);
+            grid1.Children.Add(wd.PropertyInspectorView);
         }
     }
 }
